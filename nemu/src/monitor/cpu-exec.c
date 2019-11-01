61b34ee80b89f5f4b14accaf79f04b76e108f33a
张丘洋 <cs.qyzhang@gmail.com>
2019-11-01 11:16:55 +0800

    修改了bugbox与前后文的间距；编写了一部分报告

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -46,7 +46,7 @@ void cpu_exec(uint64_t n) {
      * instruction decode, and the actual execution. */
     __attribute__((unused)) vaddr_t seq_pc = exec_once();
 
-#if defined(DIFF_TEST)
+#ifdef DIFF_TEST
     difftest_step(ori_pc, cpu.pc);
 #endif
 
